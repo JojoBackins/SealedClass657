2950d2e244c7c2015729e0762a37c2a6a3e8a4b9
jojogreen <jojogreen1408@gmail.com>
2024-03-04 17:38:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        OrdinaryHuman steve = new OrdinaryHuman(185);
+        Console.WriteLine(steve.BreakWalls(89.2)); //metod расширения
+    }
+}
+static class AmazeballsSerum
+{
+    public static string BreakWalls(this OrdinaryHuman h, double wallDensity)
+    {
+        return ($"I broke through a wall of {wallDensity} density.");
+    }
+}
+sealed class OrdinaryHuman
+{
+    private int age;
+    int weight;
+    public OrdinaryHuman(int weight)
+    {
+        this.weight = weight;
+    }
+    public void GoToWork() { }
+    public void PayBills() { }
+}
